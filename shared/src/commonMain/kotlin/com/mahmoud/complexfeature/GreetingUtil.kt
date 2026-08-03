6efe7b8f188a6406bb9e595e0e50e03a6b09acf2
Mahmoud Elnahas <mahmoud.elnahas@i-designhub.com>
2026-08-03 12:00:23 +0300

    refactor: remove unused content and simplify App function

--- shared/src/commonMain/kotlin/com/mahmoud/complexfeature/GreetingUtil.kt
+++
@@ -1,4 +0,0 @@
-package com.mahmoud.complexfeature
-
-fun sayHello(to: String): String =
-    "Hello, $to!"
--- shared/src/commonMain/kotlin/com/mahmoud/complexfeature/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.mahmoud.complexfeature
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
